7e3841c119c3e71de4877939347f094ee7e2cc78
Renogodswill <114858346+Renogodswill@users.noreply.github.com>
2022-12-30 11:32:00 +0000

    Add files via upload

---
+++ blogg/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from .models import Post, Comment
+from .models import Contact
+
+
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Post
+
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ("status",)
+    search_fields = ['title']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
+
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Contact)
+
+# Register your models here.
